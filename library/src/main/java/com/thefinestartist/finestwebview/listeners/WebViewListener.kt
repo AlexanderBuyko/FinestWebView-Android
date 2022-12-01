ca62dd196c3484c1c59041ec529eb4645c55a7e4
Alexander Buyko <>
2022-12-01 11:24:19 +0300

    Refactoring

--- library/src/main/java/com/thefinestartist/finestwebview/listeners/WebViewListener.kt
+++ library/src/main/java/com/thefinestartist/finestwebview/listeners/WebViewListener.kt
@@ -4,14 +4,14 @@ package com.thefinestartist.finestwebview.listeners
  * Created by TheFinestArtist on 1/26/16.
  */
 abstract class WebViewListener {
-  fun onProgressChanged(progress: Int) {}
-  fun onReceivedTitle(title: String?) {}
-  fun onReceivedTouchIconUrl(url: String?, precomposed: Boolean) {}
-  fun onPageStarted(url: String?) {}
-  fun onPageFinished(url: String?) {}
-  fun onLoadResource(url: String?) {}
-  fun onPageCommitVisible(url: String?) {}
-  fun onDownloadStart(
+  open fun onProgressChanged(progress: Int) {}
+  open fun onReceivedTitle(title: String?) {}
+  open fun onReceivedTouchIconUrl(url: String?, precomposed: Boolean) {}
+  open fun onPageStarted(url: String?) {}
+  open fun onPageFinished(url: String?) {}
+  open fun onLoadResource(url: String?) {}
+  open fun onPageCommitVisible(url: String?) {}
+  open fun onDownloadStart(
     url: String?,
     userAgent: String?,
     contentDisposition: String?,
